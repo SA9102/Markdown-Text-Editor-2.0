34f7729b05bfd63252a57956a58bee8e9b942163
Shayan Ali <shayan677@gmail.com>
2024-06-03 15:20:36 +0100

    Cors change.

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ require("./config/passport");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://Markdown-Text-Editor-2.0.onrender.com", credentials: true }));
 app.use(
   session({
     secret: "my secret",
